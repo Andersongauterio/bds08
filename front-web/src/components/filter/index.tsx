1818d1beae8314d5953b1a7fb6ea0fc9d8340b87
Andersongauterio <anderson.camara.gauterio@gmail.com>
2022-11-27 10:53:18 -0300

    filter backend integration

--- front-web/src/components/filter/index.tsx
+++ front-web/src/components/filter/index.tsx
@@ -1,9 +1,35 @@
+import { AxiosRequestConfig } from 'axios';
+import { useEffect, useState } from 'react';
+import Select from 'react-select';
+import { Store } from '../../types/store';
+import { makeRequest } from '../../utils/request';
 import './styles.css';
 
 const Filter = () => {
+
+    const [selectStores, setSelectStores] = useState<Store[]>([]);
+
+    useEffect(() => {
+        makeRequest
+            .get<Store[]>('/stores', {})
+            .then((response) => {
+                setSelectStores(response.data);
+            })
+            .catch(() => {
+                console.log('Error to fetch stores');
+            });
+    }, [])
+
     return (
         <div className='sales-filter-container'>
-            <h1>Filter</h1>
+            <Select
+                options={selectStores}
+                isClearable
+                placeholder="Lojas"
+                classNamePrefix="sales-filter-select"
+                getOptionLabel={(store: Store) => store.name}
+                getOptionValue={(store: Store) => String(store.id)}
+            />
         </div>
     );
 };
---
+++ front-web/src/utils/request.ts
@@ -0,0 +1,18 @@
+import  axios, { AxiosRequestConfig }  from "axios";
+import { FilterData } from "../types/filterDate";
+
+const baseURL = 'http://localhost:8080';
+
+export const makeRequest = axios.create({
+  baseURL
+});
+
+export const buildFilterParams = (
+  filterData?: FilterData,
+  extraParams?: Record<string, unknown>
+) => {
+  return {
+    gender: filterData?.store,
+    ...extraParams
+  };
+};
